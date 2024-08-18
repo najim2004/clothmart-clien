682f17ac52f4195cfa251e4d3384ce04018364a1
najim2004 <najim10112004@gmail.com>
2024-08-18 19:52:14 +0600

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,37 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Shared/Footer";
 import Navbar from "./Shared/Navbar";
 import { Toaster } from "react-hot-toast";
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import { MyContext } from "./Context/ContextProvider";
 
 function App() {
+  const { authenticationStatus } = useContext(MyContext);
+  const { isLoading } = authenticationStatus;
   const { pathname } = useLocation();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+  useEffect(() => {
+    if (!isLoading) setLoading(false);
+  }, [isLoading]);
   return (
     <div className="flex flex-col w-full lg:min-h-screen">
-      <Navbar />
-      <div className="flex-grow px-4 lg:px-0">
-        <Outlet />
-      </div>
-      <Footer />
-      <Toaster />
+      {loading ? (
+        <div className="w-full h-screen my-5 flex justify-center items-center">
+          <span className="loading loading-dots loading-lg text-gray-500"></span>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <div className="flex-grow px-4 lg:px-0">
+            <Outlet />
+          </div>
+          <Footer />
+          <Toaster />
+        </>
+      )}
     </div>
   );
 }
--- src/Pages/Home/Sections/BestSell.jsx
+++ src/Pages/Home/Sections/BestSell.jsx
@@ -7,7 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 const BestSell = () => {
   const axiosPublic = useAxios();
   // Fetch best sell products from API
-  const { data: products } = useQuery({
+  const { data: products, isLoading } = useQuery({
     queryKey: ["bestSell"],
     queryFn: async () => {
       const response = await axiosPublic.get("/products");
@@ -16,20 +16,28 @@ const BestSell = () => {
   });
   return (
     <Section className="mt-10 flex flex-col items-center">
-      <h3 className="text-[40px] font-bold mx-auto text-center mb-7">
-        Best Sell Product
-      </h3>
-      <div className="grid sm:grid-cols-2 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-7">
-        {products?.slice(0, 8).map((product) => (
-          <ProductCard key={product?._id} product={product} />
-        ))}
-      </div>
-      <Link
-        to={"/shop"}
-        className="px-5 py-1 text-lg font-medium border border-black rounded-md mx-auto mt-12"
-      >
-        Load More
-      </Link>
+      {isLoading ? (
+        <div className="w-full my-5 flex justify-center items-center">
+          <span className="loading loading-dots loading-lg text-gray-500"></span>
+        </div>
+      ) : (
+        <>
+          <h3 className="text-[40px] font-bold mx-auto text-center mb-7">
+            Best Sell Product
+          </h3>
+          <div className="grid sm:grid-cols-2 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-7">
+            {products?.slice(0, 8).map((product) => (
+              <ProductCard key={product?._id} product={product} />
+            ))}
+          </div>
+          <Link
+            to={"/shop"}
+            className="px-5 py-1 text-lg font-medium border border-black rounded-md mx-auto mt-12"
+          >
+            Load More
+          </Link>
+        </>
+      )}
     </Section>
   );
 };
